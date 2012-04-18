f72bcd3cacc77ee4f0b2b7b6cbbcf6d6342b976c
Geoff Davis <gadavis@ucsd.edu>
2012-04-17 19:23:24 -0700

    Put in safe_hiera to help bootstrap hiera

--- manifests/centos62.pp
+++ manifests/centos62.pp
@@ -1,8 +1,13 @@
 # Puppet class for CentOS 6.2
 # This is for development of zfsonlinux on CentOS 6.2
 
+require 'stdlib'
 #class { 'hiera' : }
-##Class['hiera'] -> Class['staging']
 
-# Fire up zfsonlinux
-class { 'zfsonlinux::install' : }
+if function_available('hiera') and safe_hiera('hiera_enabled', false) {
+    # Fire up zfsonlinux
+    #    class { 'zfsonlinux::install' : }
+    notice 'We think we have hiera'
+} else {
+  notice 'no hiera yet'
+}
